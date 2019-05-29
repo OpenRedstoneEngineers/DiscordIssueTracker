a762eb8a194566d7e80a6a91a39059ab737b8f4c
Omar Saad <omar.saad0705@gmail.com>
2019-05-29 03:03:09 +0200

    no need for an example serverconfig as it should start empty regardless.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const logger = winston.createLogger({
 });
 
 if (!fs.existsSync('serverconfig.json')) {
-    fs.copyFileSync('serverconfig.example.json', 'serverconfig.json');
+    fs.writeFileSync('serverconfig.json', '{}');
 }
 const serverConfig = require('./serverconfig.json');
 const commands = {
